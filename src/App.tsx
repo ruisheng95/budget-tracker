46ae30294817cdfaf0afd9047a3a1515c63e0deb
Ethan Lim <ethanlim@Ethans-MacBook-Pro.local>
2026-05-30 15:56:53 +0100

    completed settings button

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,16 @@ import { exportToBudgetCsv, importFromBudgetCSV } from "./utils/budget-parser";
 import Big from "big.js";
 import useLocalStorageData from "./utils/useLocalStorageData";
 import { getAmountTextColor } from "./utils/common";
+import { FiSettings, FiPlus} from "react-icons/fi"; //for icons
 
 function App() {
   const [accountsJson, setAccounts] = useLocalStorageData();
   const [selectedAccountId, setSelectedAccountId] = useState<
     number | undefined
   >(undefined);
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [showAddAccount, setShowAddAccount] = useState(false);
 
   const accounts = JSON.parse(accountsJson) as Account[];
   const selectedAccount = accounts.find((c) => c.id === selectedAccountId);
@@ -24,6 +28,81 @@ function App() {
       <div className="flex items-center mb-6 justify-between text-2xl font-bold">
         <h1>My Accounts</h1>
         <h1 className={getAmountTextColor(balanceSum)}>RM{balanceSum}</h1>
+        <button
+          className="bg-gray-300 px-4 py-2 rounded hover:bg-gray-400"
+          onClick={() => {
+            setShowSettings(true);
+          }}
+        >
+          <FiSettings size={24} />
+        </button>
+
+        {/* settings popup */}
+        {showSettings && (
+          <div
+            className="fixed inset-0 bg-black/20 backdrop-blur-sm flex items-center justify-center"
+            onClick={() => {
+              setShowSettings(false);
+            }}
+          >
+            <div
+              className="bg-white p-5 rounded-lg shadow-lg w-96"
+              onClick={(e) => {
+                e.stopPropagation(); // block click from propagating to the outer div when pressing settings stuff
+              }}
+            >
+              <h2 className="text-2xl font-bold mb-4">Settings</h2>
+              <div className="flex gap-4">
+                <button
+                  className="text-xl bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                  onClick={() => {
+                    exportToBudgetCsv(accounts);
+                  }}
+                >
+                  Export CSV
+                </button>
+
+                <label className="text-center text-xl bg-gray-200 px-4 py-2 rounded cursor-pointer hover:bg-gray-300">
+                  Import CSV
+                  <input
+                    type="file"
+                    accept=".csv"
+                    onChange={(e) => {
+                      importFromBudgetCSV(e)
+                        .then((result) => {
+                          localStorage.setItem("data", JSON.stringify(result));
+                          setAccounts(result);
+                          // if (navigator.storage?.persist) {
+                          //   const isPersisted = await navigator.storage.persist();
+                          //   console.log(`Persisted storage granted: ${isPersisted}`);
+                          // }
+                        })
+                        .catch((e: unknown) => {
+                          console.error("Import failed:", e);
+                        });
+                    }}
+                    className="hidden"
+                  />
+                </label>
+
+                <button
+                  className="text-xl bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+                  onClick={() => {
+                    setAccounts([]);
+                    localStorage.clear();
+                    setShowSettings(false);
+                  }}
+                >
+                  Clear All Data
+                </button>
+              </div>
+              <p className="text-gray-600 text-xs italic mt-4">
+                Click anywhere in the background to close
+              </p>
+            </div>
+          </div>
+        )}
+        {/* End settings popup */}
       </div>
       <div className="grid gap-4">
         {accounts.map((account) => (
@@ -36,7 +115,9 @@ function App() {
           >
             <h2 className="text-lg font-semibold">{account.name}</h2>
             <p
-              className={`text-2xl font-mono ${getAmountTextColor(account.balance)} flex justify-between`}
+              className={`text-2xl font-mono ${getAmountTextColor(
+                account.balance
+              )} flex justify-between`}
             >
               <span>RM{account.balance.toFixed(2)}</span>
               {account.name === "Shopee" && (
@@ -50,53 +131,88 @@ function App() {
             </p>
           </div>
         ))}
-      </div>
-      <div>&nbsp;</div>
-      <div className="flex gap-4 mb-6 mt-auto flex-col">
-        <div className="flex gap-4">
-          <button
-            className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
-            onClick={() => {
-              exportToBudgetCsv(accounts);
-            }}
-          >
-            Export CSV
-          </button>
 
-          <label className="bg-gray-200 px-4 py-2 rounded cursor-pointer hover:bg-gray-300">
-            Import CSV
-            <input
-              type="file"
-              accept=".csv"
-              onChange={(e) => {
-                importFromBudgetCSV(e)
-                  .then((result) => {
-                    localStorage.setItem("data", JSON.stringify(result));
-                    setAccounts(result);
-                    // if (navigator.storage?.persist) {
-                    //   const isPersisted = await navigator.storage.persist();
-                    //   console.log(`Persisted storage granted: ${isPersisted}`);
-                    // }
-                  })
-                  .catch((e: unknown) => {
-                    console.error("Import failed:", e);
-                  });
-              }}
-              className="hidden"
-            />
-          </label>
-
-          <button
-            className="bg-red-600 text-white px-4 py-2 rounded hover:bg-green-700"
-            onClick={() => {
-              setAccounts([]);
-              localStorage.clear();
-            }}
-          >
-            Clear All Data
-          </button>
+        {/* Add new acc button */}
+        <div
+          className="p-6 bg-white rounded-xl shadow-sm hover:shadow-md cursor-pointer transition-all border border-gray-200"
+          onClick={() => {
+            setShowAddAccount(true);
+          }}
+        >
+            <FiPlus size={24} className="mx-auto" />
         </div>
+
+        {/* Add new acc popup */}
+        {showAddAccount && (
+          <div
+            className="fixed inset-0 bg-black/20 backdrop-blur-sm flex items-center justify-center"
+            onClick={() => {
+              setShowAddAccount(false);
+            }}
+          >
+            <div
+              className="bg-white p-5 rounded-lg shadow-lg w-96"
+              onClick={(e) => {
+                e.stopPropagation(); // block click from propagating to the outer div when pressing settings stuff
+              }}
+            >
+              <h2 className="text-2xl font-bold mb-4">Add account</h2>
+
+              <p className="text-gray-600 text-xs italic mt-4">
+                Click anywhere in the background to close
+              </p>
+            </div>
+          </div>
+        )}
+
       </div>
+
+      {/* <div>&nbsp;</div>
+      <div className="flex gap-4 mb-6 mt-auto flex-col">
+      <div className="flex gap-4">
+            <button
+                className="text-xl bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                onClick={() => {
+                exportToBudgetCsv(accounts);
+                }}
+            >
+                Export CSV
+            </button>
+
+            <label className="text-xl bg-gray-200 px-4 py-2 rounded cursor-pointer hover:bg-gray-300">
+                Import CSV
+                <input
+                type="file"
+                accept=".csv"
+                onChange={(e) => {
+                    importFromBudgetCSV(e)
+                    .then((result) => {
+                        localStorage.setItem("data", JSON.stringify(result));
+                        setAccounts(result);
+                        // if (navigator.storage?.persist) {
+                        //   const isPersisted = await navigator.storage.persist();
+                        //   console.log(`Persisted storage granted: ${isPersisted}`);
+                        // }
+                    })
+                    .catch((e: unknown) => {
+                        console.error("Import failed:", e);
+                    });
+                }}
+                className="hidden"
+                />
+            </label>
+
+            <button
+                className="text-xl bg-red-600 text-white px-4 py-2 rounded hover:bg-green-700"
+                onClick={() => {
+                setAccounts([]);
+                localStorage.clear();
+                }}
+            >
+                Clear All Data
+            </button>
+        </div>
+      </div> */}
     </section>
   );
 
